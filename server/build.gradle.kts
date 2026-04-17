bdd67a898627e9bdefa1731390740055731ccba4
wiyan <wianwiryawan@gmail.com>
2026-04-17 12:42:16 +0700

    Initial commit for server side

---
+++ server/build.gradle.kts
@@ -0,0 +1,61 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import org.gradle.api.tasks.testing.logging.TestLogEvent.*
+
+plugins {
+  java
+  application
+  id("com.gradleup.shadow") version "9.2.2"
+}
+
+group = "com.dashboard.pascabencana"
+version = "1.0.0-SNAPSHOT"
+
+repositories {
+  mavenCentral()
+}
+
+val vertxVersion = "5.0.10"
+val junitJupiterVersion = "5.9.1"
+
+val mainVerticleName = "com.vertx.dashboard.pascabencana.MainVerticle"
+val launcherClassName = "io.vertx.launcher.application.VertxApplication"
+
+application {
+  mainClass.set(launcherClassName)
+}
+
+dependencies {
+  implementation(platform("io.vertx:vertx-stack-depchain:$vertxVersion"))
+  implementation("io.vertx:vertx-launcher-application")
+  implementation("io.vertx:vertx-auth-jwt")
+  implementation("io.vertx:vertx-pg-client")
+  implementation("io.vertx:vertx-json-schema")
+  implementation("io.vertx:vertx-shell")
+  testImplementation("io.vertx:vertx-junit5")
+  testImplementation("org.junit.jupiter:junit-jupiter:$junitJupiterVersion")
+  testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+java {
+  sourceCompatibility = JavaVersion.VERSION_21
+  targetCompatibility = JavaVersion.VERSION_21
+}
+
+tasks.withType<ShadowJar> {
+  archiveClassifier.set("fat")
+  manifest {
+    attributes(mapOf("Main-Verticle" to mainVerticleName))
+  }
+  mergeServiceFiles()
+}
+
+tasks.withType<Test> {
+  useJUnitPlatform()
+  testLogging {
+    events = setOf(PASSED, SKIPPED, FAILED)
+  }
+}
+
+tasks.withType<JavaExec> {
+  args = listOf(mainVerticleName)
+}
